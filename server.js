cf9eb766d841cb59288909da935163e54ba4230e
Alvin Yearby <leroy.yearby@gmail.com>
2021-12-15 00:23:02 -0500

    changed port

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express'); 
+const PORT = process.env.PORT || 3001; 
 const app = express(); 
 const { animals } = require('./data/animals.json'); 
 
@@ -40,7 +41,7 @@ function filterByQuery(query, animalsArray) {
     // return the filtered results:
     return filteredResults;
   }
-  
+
 
 
 app.get('/api/animals', (req, res) => {
@@ -52,7 +53,7 @@ app.get('/api/animals', (req, res) => {
   });
 
 
-app. listen(3001, () => {
-    console.log('API server now on port 3001!'); 
-}); 
+  app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  }); 
 
